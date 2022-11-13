8c2bb7b0d2535362e00b06c3043aa7920f7981d4
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-11-13 11:46:47 +0100

    task2

--- task_2.js
+++ task_2.js
@@ -1,27 +1,32 @@
 // function to check if a word is a palindrome
 // 1.
-function checkWordIsPalindrome(word){
+// function checkWordIsPalindrome(word){
  
-let i, j;
+// let i, j;
  
-for (i = 0, j = word.length-1; i < j; i++, j--){
-    if (word[i] != word[j]){
-        break;
-    }
-  }
-    if (i < j){
-        console.log("The word isn`t a palindrome");
-    } else {
-        console.log("The word is a palindrome");
-    }
-}
-
+// for (i = 0, j = word.length-1; i < j; i++, j--){
+//     if (word[i] != word[j]){
+//         break;
+//     }
+//   }
+//     if (i < j){
+//         console.log("The word isn`t a palindrome");
+//     } else {
+//         console.log("The word is a palindrome");
+//     }
+// }
+// checkWordIsPalindrome("jhhfgfd");
+// checkWordIsPalindrome("anna");
 
 // 2
-// function checkWordIsPalindrome(word)
 
-checkWordIsPalindrome("jhhfgfd");
-checkWordIsPalindrome("anna");
+// let test = "aba"
+// console.log(test.split("").reverse().join("") === test)
 
-let test = "aba"
-console.log(test.split("").reverse().join("") === test)
+// 3
+function isPalindrome(word){
+    console.log(word.split("").reverse().join("") === word);
+}
+
+isPalindrome("abc");
+isPalindrome("aba");
