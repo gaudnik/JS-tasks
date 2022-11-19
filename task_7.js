450db58a31714d80ce23ddbebf0b99ace4a5f70a
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-11-19 17:32:53 +0100

    task7

---
+++ task_7.js
@@ -0,0 +1,11 @@
+// write a function that return object containing 
+// key and value: how many times letter appear in string.
+
+function countLetter(word){
+    const arr= []
+      arr.push(word.split(""));
+  
+      return arr;
+  }
+  
+  console.log(countLetter("zuza"));
