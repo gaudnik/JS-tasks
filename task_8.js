2bbf629b2ed03136b88233ea4eec103bfe1c18c4
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-11-20 10:32:09 +0100

    task8

--- task_8.js
+++ task_8.js
@@ -1,11 +1,14 @@
-// const sentence = list => (
-//     list
-//       .sort((a, b) => Object.keys(a)[0] - Object.keys(b)[0])
-//       .map(obj => Object.values(obj)[0])
-//       .join(' ')
-//   );
+// The input will be an array of dictionaries.
 
-function sentence(list){
-    list.sort((a,b) => Object.keys(a)[0] - Object.keys(b)[0])
-    .map(obj => Object.values(obj)[0]).join(' ')
-}
+// Return the values as a string-seperated sentence in the order of their keys' 
+// integer equivalent (increasing order).
+
+// The keys are not reoccurring and their range is -999 < key < 999. 
+// The dictionaries' keys & values will always be strings and will always not be empty.
+
+const sentence = list => (
+    list
+      .sort((a, b) => Object.keys(a)[0] - Object.keys(b)[0])
+      .map(obj => Object.values(obj)[0])
+      .join(' ')
+  );
