77b02205b70082e344ba4dca3f6c990a61f31041
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-11-23 21:03:48 +0100

    task18

---
+++ task_18.js
@@ -0,0 +1,14 @@
+function isIsogram(str) {
+    str = str.toLowerCase()
+    
+    for (let i = 0; i < str.length; i++) {
+      if (str.indexOf(str.charAt(i), i + 1) >= 0) {
+        return false
+      }
+    }
+    
+    return true
+  }
+
+  console.log(isIsogram("Dermatoglyphics"));
+
