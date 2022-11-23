8944779c315c5385fdab81c5ad325fccbf7ac4ec
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-11-23 12:40:37 +0100

    refactor

--- task_15.js
+++ task_15.js
@@ -1,8 +1,1 @@
-const sumOfABeach = (beach) => {
-beach = beach.toLowerCase();
-   let sand = beach.split("sand").length - 1,
-       water = beach.split("water").length - 1,
-       fish = beach.split("fish").length - 1,
-       sun = beach.split("sun").length - 1
-  return sand + water + fish + sun;
-}
+const sumOfABeach = beach => (beach.match(/sand|water|fish|sun/ig) || []).length
