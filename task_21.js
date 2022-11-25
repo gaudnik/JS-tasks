da4718aebb4c921d6d763f8f6a9822dd58ef8c49
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-11-25 16:48:28 +0100

    task21

---
+++ task_21.js
@@ -0,0 +1,12 @@
+function getCount(str) {
+    let vowelsCount = 0;
+    let vowels = ['a','e','i','o','u'];
+    for(let char of str){
+      if(vowels.includes(char)){
+        vowelsCount++;
+      }
+    }
+    return vowelsCount;
+  }
+
+  console.log(getCount('abrakadabra'));
