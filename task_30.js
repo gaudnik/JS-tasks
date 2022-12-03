7d1b07e78f50364c51d4fab2636e952cfa73b1fc
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-12-03 19:12:37 +0100

    task30

--- task_30.js
+++ task_30.js
@@ -1,3 +1,4 @@
 function numberJoy(n) {
-    // your code
+    let nr = (n.toString().split("").reduce((a , b) => a+ +b, 0));
+    return n - (nr * nr.toString().split("").reverse().join("")) === 0;
 }
