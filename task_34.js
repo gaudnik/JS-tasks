ed3436a399ba2fc4205ed7b474345b414d367bc4
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-12-08 17:26:03 +0100

    task34

---
+++ task_34.js
@@ -0,0 +1,15 @@
+function calcType(a, b, res) {
+    if(a+b == res){
+      return "addition"
+    }
+    if(a-b == res){
+      return "subtraction"
+    }
+    if(a*b == res){
+      return "multiplication"
+    }
+    if(a/b == res){
+      return "division"
+    }
+    return res;
+  }
