b7c8c401fbe59d773893031e22763220cf016936
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-12-19 10:29:25 +0100

    task39

---
+++ task_39.js
@@ -0,0 +1,13 @@
+const task = (w, n, c) => {
+    let workers = {
+      Monday: 'James',
+      Tuesday: 'John',
+      Wednesday: 'Robert',
+      Thursday: 'Michael',
+      Friday: 'William'
+    };
+    
+    return `It is ${w} today, ${workers[w]}, you have to work, you must spray ${n} trees and you need ${n * c} dollars to buy liquid`;
+  
+  };
+  
